7c51a614e2000549119c28fe9916f4dbd5c9488b
ASO2001040 <83265776+ASO2001040@users.noreply.github.com>
2021-07-15 10:12:45 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Systsem.out.println("戦艦ゲーム");								                              
+        Random rnd = new Random();
+        int value = rnd.nextInt(5);
+        System.out.println(value);
+    }
+}
